7e96fd4f3a198c20125554cf609919794f680b24
Keri <105250570+kwilkinz@users.noreply.github.com>
2025-03-26 06:44:48 -0500

    feat(login) base setup of api request for login method

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+
+// BASE URL
+const API_BASE_URL = "https://frontend-take-home-service.fetch.com";
+
+// Items sent with request
+
+const login = () => {
+  try {
+    // post
+  } catch (error) {
+    console.log("Login at api.js failed: ", error);
+    throw error;
+  }
+};
